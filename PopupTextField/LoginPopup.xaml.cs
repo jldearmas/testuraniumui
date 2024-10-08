5e2842f849bda6f257f5b4769fe36c12c7a2bceb
Jorge Luis De Armas García <jldearmas@gmail.com>
2024-10-08 19:16:54 -0400

    Completed code to get running the LoginPopup when the bug be fixed

--- PopupTextField/LoginPopup.xaml.cs
+++ PopupTextField/LoginPopup.xaml.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Maui.Views;
+using System.Windows.Input;
 
 namespace PopupTextField;
 
@@ -7,7 +8,16 @@ public partial class LoginPopup : Popup
 	public LoginPopup()
 	{
 		InitializeComponent();
+        
+		SubmitCmd = new Command<bool>(OnSubmit);
 
+		BindingContext = this;
     }
 
+	private void OnSubmit(bool isValid)
+	{
+		this.Close();
+	}
+
+    public ICommand SubmitCmd { get; private set; }
 }
